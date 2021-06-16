5d63825b500d26357325721e17875925be190ef8
André <andre.gmaps@gmail.com>
2021-06-15 21:06:40 -0300

    applying ModelAndView class

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,28 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
 	@GetMapping(path = "/home")
-	public String home() {
-		return "home";
+	public ModelAndView home() {
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("pedidos", pedidos);
+		
+		return modelAndView;
 	}
 }
