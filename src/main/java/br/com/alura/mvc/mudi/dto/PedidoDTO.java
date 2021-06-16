ccade32b58078ee394abaf1c69fae69e8a754752
André <andre.gmaps@gmail.com>
2021-06-16 20:20:50 -0300

    creating PedidoDTO

---
+++ src/main/java/br/com/alura/mvc/mudi/dto/PedidoDTO.java
@@ -0,0 +1,15 @@
+package br.com.alura.mvc.mudi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PedidoDTO {
+	
+	private String nomeProduto;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+}
