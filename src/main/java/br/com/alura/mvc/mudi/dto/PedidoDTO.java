11f1286ba02d73f73d9d4d9af536fbcbb5742308
André <andre.gmaps@gmail.com>
2021-06-17 21:42:48 -0300

    changing annotations

--- src/main/java/br/com/alura/mvc/mudi/dto/PedidoDTO.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/PedidoDTO.java
@@ -1,10 +1,8 @@
 package br.com.alura.mvc.mudi.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class PedidoDTO {
 	
 	private String nomeProduto;
