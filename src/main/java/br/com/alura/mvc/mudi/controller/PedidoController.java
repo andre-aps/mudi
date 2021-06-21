bf38596e27e98ab420288654be3df80dfa24064d
André <andre.gmaps@gmail.com>
2021-06-21 19:39:56 -0300

    creating Pedido controller

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,38 @@
+package br.com.alura.mvc.mudi.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.PedidoDTO;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("/pedido")
+public class PedidoController {
+
+	@Autowired
+	PedidoRepository pedidoRepository;
+
+	@GetMapping("/formulario")
+	public String formulario(PedidoDTO pedidoDTO) {
+		return "pedido/formulario";
+	}
+
+	@PostMapping("/novo")
+	public String novo(@Valid PedidoDTO pedidoDTO, BindingResult result) {
+		if(result.hasErrors())
+			return "pedido/formulario";
+		
+		Pedido pedido = Pedido.toModel(pedidoDTO);
+		pedidoRepository.save(pedido);
+		return "pedido/formulario";
+	}
+
+}
