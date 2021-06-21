934ccc513c5389349383e300ea71366b1e5cec4e
André <andre.gmaps@gmail.com>
2021-06-21 19:41:32 -0300

    adding annotations

--- src/main/java/br/com/alura/mvc/mudi/dto/PedidoDTO.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/PedidoDTO.java
@@ -1,12 +1,17 @@
 package br.com.alura.mvc.mudi.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class PedidoDTO {
 	
+	@NotBlank
 	private String nomeProduto;
+	@NotBlank
 	private String urlProduto;
+	@NotBlank
 	private String urlImagem;
 	private String descricao;
 	
