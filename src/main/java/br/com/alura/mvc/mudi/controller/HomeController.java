40896d38946ad1a5510c4ee63794e9c6087c5801
André <andre.gmaps@gmail.com>
2021-06-24 22:43:50 -0300

    creating status endpoint

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -4,25 +4,42 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	PedidoRepository pedidoRepository;
 	
-	@GetMapping(path = "/home")
+	@GetMapping
 	public ModelAndView home() {
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		
-		ModelAndView modelAndView = new ModelAndView("home");
-		modelAndView.addObject("pedidos", pedidos);
+		return new ModelAndView("home")
+				.addObject("pedidos", pedidos);
+	}
+	
+	@GetMapping("/{status}")
+	public ModelAndView byStatus(@PathVariable("status") String status) {
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
 		
-		return modelAndView;
+		return new ModelAndView("home")
+				.addObject("pedidos", pedidos)
+				.addObject(status);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 }
