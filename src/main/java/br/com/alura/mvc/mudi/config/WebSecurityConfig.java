9002304faa1bfe3a2b66a47d1e5a0c3a2500a09f
André <andre.gmaps@gmail.com>
2021-07-06 21:34:43 -0300

    adding default success url

--- src/main/java/br/com/alura/mvc/mudi/config/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.anyRequest().authenticated()
-			.and().formLogin(form -> form.loginPage("/login").permitAll())
+			.and().formLogin(form -> form.loginPage("/login").permitAll().defaultSuccessUrl("/home", true))
 			.logout(logout -> logout.logoutUrl("/logout"))
 			.csrf().disable();
 	}
