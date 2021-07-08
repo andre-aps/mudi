68fd1ef1efe840152441722e5d72add8fb2ac5ad
André <andre.gmaps@gmail.com>
2021-07-07 22:45:53 -0300

    creating Usuario class

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Usuario.java
@@ -0,0 +1,28 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class Usuario {
+	
+	@Id
+	private String username;
+	private String password;
+	private Boolean enabled;
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Pedido> pedidos;
+	
+}
