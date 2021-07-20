846c67067c448b9af0737553ba4568ea2eeb3ede
André <andre.gmaps@gmail.com>
2021-07-19 21:00:00 -0300

    enabling cache

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+	@Cacheable("pedidosEntregue")
 	List<Pedido> findByStatus(StatusPedido status, Pageable page);
 
 	List<Pedido> findByStatusAndUsuario_username(StatusPedido status, String username);
